dedf0cdea87cc9e7958d1d95bb8115a226a72886
persank <persank@gmail.com>
2026-02-28 23:44:06 -0500

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,15 @@
+from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage
+
+model = ChatOllama(
+    model="llama3.2:latest",
+    temperature=0.8,
+)
+
+messages = [
+    SystemMessage(content="You are a math expert"),
+    HumanMessage(content="What is the square root of 324 plus 45?")
+]
+
+response = model.invoke(messages)
+print("Response:", response)
